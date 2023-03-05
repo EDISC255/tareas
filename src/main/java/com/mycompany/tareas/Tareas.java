07e7a156579a26e2be021c785905e2d1eb9f60f9
Eduardo Cabrera Montero <isc.eduardo.came@gmail.com>
2023-03-05 08:00:05 -0600

    respaldo

---
+++ src/main/java/com/mycompany/tareas/Tareas.java
@@ -0,0 +1,10 @@
+package com.mycompany.tareas;
+
+public class Tareas{
+    public static void main(String[] args) {
+        ListaTareas l= new ListaTareas();
+        
+        l.setVisible(true);
+        l.setLocationRelativeTo(null);
+    }
+}
